35cdbddc1f4861b93e2d99b76e2d25a1397420f1
Ivana Vasileva <106319705+IvanaAntonieva@users.noreply.github.com>
2023-01-17 23:53:46 +0200

    Delete Java Advanced/src directory

--- Java Advanced/src/Ex_01_FillTheMatrix.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Ex_01_FillTheMatrix {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        
-    }
-}
